c510aa8679e39c1c940277c305f8d056375147bc
EdwardGa0 <edwardgaobot@gmail.com>
2023-04-05 20:51:37 -0400

    completed basic functionality

--- sim.py
+++ sim.py
@@ -14,21 +14,19 @@ class Roundabout:
     def enter(self, enter_dir, exit_dir):
         quarter_index = self._direction_to_quarter_index(enter_dir)
         # Store the exit direction in quarters
-        self.quarters[quarter_index].append(self._direction_to_quarter_index(exit_dir))
-    
-    def exit(self):
-        for quarter_index, quarter in enumerate(roundabout.quarters):
-            if quarter[0] == quarter_index:
-                quarter.popleft()
+        self.quarters[quarter_index].append(self._direction_to_quarter_index(exit_dir))  
     
     def update(self):
-        # Front car either exits, or moves to next quarter
-        # Try to enter car(s)
-        pass
+        for quarter_index, quarter in enumerate(roundabout.quarters):
+            if quarter:
+                first = quarter.popleft()
+                if first != quarter_index:
+                    # Move first to next quarter
+                    roundabout.quarters[quarter_index - 1].append(first)
 
-    def display(self):
-        # Print out the timestamp and self.quarters
-        pass
+
+    def display(self, time):
+        print('t =', time, 'N:', list(self.quarters[0]), 'E:', list(self.quarters[1]), 'S:', list(self.quarters[2]), 'W:', list(self.quarters[3]))
 
     def _direction_to_quarter_index(self, direction):
         if direction == 'N':
@@ -45,18 +43,22 @@ n = int(input())
 
 roundabout = Roundabout(size)
 
-cars = []
+cars = deque()
 for i in range(n):
     entry_time, entry_dir, exit_dir = input().split()
     cars.append((int(entry_time), entry_dir, exit_dir))
 
-cars.sort()
-
 time = 0
-car_index = 0
 
-while car_index < n or any(roundabout.quarters):
-    roundabout.exit()
+while cars or any(roundabout.quarters):
     roundabout.update()
-    roundabout.display()
+    car_index = 0
+    # loop through waiting cars based on arrival time
+    while car_index < len(cars) and time >= cars[car_index][0]:
+        if roundabout.can_enter(cars[car_index][1]):
+            roundabout.enter(cars[car_index][1], cars[car_index][2])
+            del cars[car_index]
+        else:
+            car_index += 1
+    roundabout.display(time)
     time += 1
